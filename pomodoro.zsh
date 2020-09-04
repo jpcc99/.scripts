d1b1dab8b117b193041864295384094761c4ba1d
jpcc99 <jpcc.dev@gmail.com>
2020-09-03 22:55:05 -0300

    Break the code into short functions

--- pomodoro.zsh
+++ pomodoro.zsh
@@ -5,9 +5,26 @@
 #	Description:
 #       A minimalistic pomodoro script
 
-notify-send '🍅 Pomodoro' 'Stay focused! 🎯'
-sleep 1500 &&  notify-send '🍅 Pomodoro' '25 minutes passed' \
-    && paplay /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
-# Short pause
-sleep 300 && notify-send '🍅 Pomodoro' 'Back to work!' \
-    && paplay /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
+# TODO Options to choose beteween: do a pause or just count a 25'
+# TODO Choose how many cycles you wanto to do
+
+function start_cycle() {
+    start_pomodoro
+    do_pause
+}
+
+function start_pomodoro() {
+    notify-send '🍅 Pomodoro' 'Stay focused! 🎯'
+    $(sleep 1500 &&  notify-send '🍅 Pomodoro' '25 minutes passed' \
+        && paplay /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga)
+
+}
+
+function do_pause() {
+    # Short pause
+    $(sleep 300 && notify-send '🍅 Pomodoro' 'Back to work!' \
+        && paplay /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga)
+
+}
+
+start_cycle
